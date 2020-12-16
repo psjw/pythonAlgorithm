064170436b38be276229188ef73a84d6b3c0fbfa
이진우 <dlrudtn108@gmail.com>
2020-12-16 23:11:49 +0900

    파이썬 알고리즘

---
+++ ex3.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin=open('ex3.txt','rt')
+n,k=map(int,input().split())
+
+a=list(map(int,input().split()))
+result=set()
+
+for i in range(n):
+    for j in range(i+1,n): 
+        for m in range(j+1,n) :          
+            result.add(a[i]+a[j]+a[m])
+
+result=list(result)
+result.sort(reverse=True)
+print(result[k-1])
+#print(sorted(result)[len(result)-k])    
---
+++ ex4_1.py
@@ -0,0 +1,25 @@
+import sys
+import math
+sys.stdin=open('ex4.txt','rt')
+
+n = input().split()
+result=list(map(int,input().split()))
+# 올림 math.ceil
+# 내림 math.trunc
+#     math.floor
+# 반올림 round
+
+avg=round(sum(result)/n)
+
+resultMinus=[abs(i-avg) for i in result]
+resultIndex=[x for x in range(len(resultMinus)) if abs(resultMinus[x])==min(resultMinus)]
+resultValue=[result[z] for z in resultIndex]
+print(avg,result.index(max(resultValue))+1)
+
+
+#print(list(filter(lambda x:abs(resultMinus[x])==1, range(len(resultMinus)))))
+
+
+        
+        
+
